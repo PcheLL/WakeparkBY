9300d2b4aef25da9ffe61586fe5f069dc2df03d6
Ilya Pchelnikov <alexmonts1398@gmail.com>
2019-01-08 11:25:08 +0300

    Activity: Create Account, Sign In.

--- app/src/main/java/com/example/wakeparkby/pchell/wakeparkproject/SignIn.java
+++ app/src/main/java/com/example/wakeparkby/pchell/wakeparkproject/SignIn.java
@@ -1,4 +1,25 @@
 package com.example.wakeparkby.pchell.wakeparkproject;
 
-public class SignIn {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SignIn extends AppCompatActivity implements View.OnClickListener{
+    private Button buttEnter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        buttEnter = (Button) findViewById(R.id.buttonSignIn);
+        buttEnter.setOnClickListener((View.OnClickListener) this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent_MainMenu = new Intent(this, MainMenu.class);
+        startActivity(intent_MainMenu);
+    }
 }
--- app/src/main/java/com/example/wakeparkby/pchell/wakeparkproject/Welcome.java
+++ app/src/main/java/com/example/wakeparkby/pchell/wakeparkproject/Welcome.java
@@ -1,13 +1,37 @@
 package com.example.wakeparkby.pchell.wakeparkproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Welcome extends AppCompatActivity {
+public class Welcome extends AppCompatActivity implements View.OnClickListener{
+    private Button buttSignIn;
+    private Button buttCreateAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        buttSignIn = (Button) findViewById(R.id.buttonSignIn);
+        buttCreateAcc = (Button) findViewById(R.id.buttonCreateAcc);
+        buttSignIn.setOnClickListener((View.OnClickListener) this);
+        buttCreateAcc.setOnClickListener((View.OnClickListener) this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent_CreateAcc = new Intent(this, CreateAccount.class);
+        Intent intent_SignIn = new Intent(this, SignIn.class);
+        switch (v.getId()) {
+            case R.id.buttonCreateAcc:
+                startActivity(intent_CreateAcc);
+                break;
+            case R.id.buttonSignIn:
+                startActivity(intent_SignIn);
+                break;
+        }
     }
 }
