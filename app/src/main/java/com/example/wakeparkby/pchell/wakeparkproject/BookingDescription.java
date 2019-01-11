24625542480eb1cffdbe39617d8f44b3e88233bd
Ilya Pchelnikov <alexmonts1398@gmail.com>
2019-01-11 12:47:39 +0300

    Database entry

--- app/src/main/java/com/example/wakeparkby/pchell/wakeparkproject/BookingDescription.java
+++ app/src/main/java/com/example/wakeparkby/pchell/wakeparkproject/BookingDescription.java
@@ -1,11 +1,19 @@
 package com.example.wakeparkby.pchell.wakeparkproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class BookingDescription extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BookingDescription extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRefReserved = database.getReference("RESERVED");
     private TextView textViewLocation;
     private TextView textViewDate;
     private TextView textViewTime;
@@ -17,6 +25,10 @@ public class BookingDescription extends AppCompatActivity {
     private static String infoPrice = null;
     private String m1 = null;
     private String m2 = null;
+    private Button buttBooking;
+    private Button buttBackToMenu;
+    private ImageView imageViewReserved;
+    private String name = "Pasha";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +42,15 @@ public class BookingDescription extends AppCompatActivity {
         textViewTime.setText(infoTime);
         textViewPrice = (TextView) findViewById(R.id.textViewInfoPrice);
         textViewPrice.setText(infoPrice);
+        buttBooking = (Button) findViewById(R.id.buttonBooking);
+        buttBooking.setOnClickListener((View.OnClickListener) this);
+        imageViewReserved = (ImageView) findViewById(R.id.imageViewReserved);
+        buttBackToMenu = (Button) findViewById(R.id.buttonBackToMenu);
+        buttBackToMenu.setOnClickListener((View.OnClickListener) this);
     }
 
     protected void infoLocation(String locationName) {
-        infoLocationName =("Location: " + locationName);
+        infoLocationName = ("Location: " + locationName);
     }
 
     protected void infoDate(int mDay, int mMonth, int mYear) {
@@ -78,19 +95,35 @@ public class BookingDescription extends AppCompatActivity {
 
     protected void infoTime(int hour1, int min1, int hour2, int min2) {
         if (min1 < 10) {
-             m1 = ("0" + min1);
-        }
-        else {
+            m1 = ("0" + min1);
+        } else {
             m1 = String.valueOf(min1);
         }
-         if (min2 < 10) {
-             m2 = ("0" + min2);
+        if (min2 < 10) {
+            m2 = ("0" + min2);
+        } else {
+            m2 = String.valueOf(min2);
         }
-        else {
-             m2 = String.valueOf(min2);
-         }
-        infoTime =("Time: " + hour1 + "." + m1 + " - " + hour2 + "." + m2);
+        infoTime = ("Time: " + hour1 + "." + m1 + " - " + hour2 + "." + m2);
     }
 
-    protected void infoPrice(){}
+    protected void infoPrice() {
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent_Main_menu = new Intent(this, MainMenu.class);
+        switch (v.getId()) {
+            case R.id.buttonBooking:
+                imageViewReserved.setVisibility(View.VISIBLE);
+                buttBooking.setVisibility(View.INVISIBLE);
+                buttBackToMenu.setVisibility(View.VISIBLE);
+                myRefReserved.child(infoData).push().setValue(infoTime + name);
+                break;
+            case R.id.buttonBackToMenu:
+                startActivity(intent_Main_menu);
+                break;
+        }
+
+    }
 }
