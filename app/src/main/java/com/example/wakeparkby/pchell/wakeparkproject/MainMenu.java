f56a77c0dff5a64b0b621921b10776cde43213c6
Ilya Pchelnikov <alexmonts1398@gmail.com>
2019-01-27 18:38:27 +0300

    Slide menu

--- app/src/main/java/com/example/wakeparkby/pchell/wakeparkproject/MainMenu.java
+++ app/src/main/java/com/example/wakeparkby/pchell/wakeparkproject/MainMenu.java
@@ -1,4 +1,4 @@
-package com.example.wakeparkby.pchell.wakeparkproject;
+/*package com.example.wakeparkby.pchell.wakeparkproject;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -55,5 +55,113 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
                 startActivity(intent_Change_Price);
                 break;
         }
+    }
+}*/
+
+package com.example.wakeparkby.pchell.wakeparkproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        //        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        //drawer.addDrawerListener(toggle);
+        //toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        Intent intent_LocationSelection = new Intent(this, LocationSelection.class);
+        Intent intent_Give_Season_Tickets = new Intent(this, GiveSeasonTickets.class);
+        Intent intent_Take_Season_Tickets = new Intent(this, TakeSeasonTickets.class);
+        Intent intent_Price = new Intent(this, Price.class);
+        Intent intent_Change_Price = new Intent(this, ChangePrices.class);
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+         if (id == R.id.nav_ChoosePl) {
+            startActivity(intent_LocationSelection);
+        } else if (id == R.id.nav_MySeasonTickets) {
+
+        } else if (id == R.id.nav_Price) {
+            startActivity(intent_Price);
+
+        } else if (id == R.id.nav_History) {
+
+        } else if (id == R.id.nav_Mark) {
+
+        } else if (id == R.id.nav_Give_Season_Tickets) {
+            startActivity(intent_Give_Season_Tickets);
+
+        } else if (id == R.id.nav_Take_Season_Tickets) {
+            startActivity(intent_Take_Season_Tickets);
+
+        } else if (id == R.id.nav_Change_Price) {
+            startActivity(intent_Change_Price);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
